e5ab285b1959b68a4eb984721f7bc895d07a0bb7
trong tin <trongtin24097735@gmail.com>
2024-10-16 22:30:09 +0700

    q3

---
+++ lab2/Q3IncomeTax/style.js
@@ -0,0 +1,28 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 28,
+    marginBottom: 10,
+  },
+  input: {
+    width: 200,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    padding: 18,
+  },
+  result: {
+    marginTop: 28,
+    fontSize: 16,
+  },
+});
+
+export default styles;
